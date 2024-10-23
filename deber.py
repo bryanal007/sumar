43329903f62b6827efd763da12d95ea1b021aeb9
bryanal007 <alexanderreal00@gmail.com>
2024-10-23 15:29:32 -0500

    Update deber.py

--- deber.py
+++ deber.py
@@ -1,12 +1,12 @@
-print("Se va a calcular el area del triángulo");
-b=float(input("Ingrese la base: "));
-h=float(input("Ingrese la altura: "));
-a=b*h/2;
+print("Se va a calcular el area del triángulo")
+b=float(input("Ingrese la base: "))
+h=float(input("Ingrese la altura: "))
+a=b*h/2
 print(f"El área del triángulo es: {a}")
 
-print("Se va a calcular el perímetro del triángulo");
-l1=float(input("Ingrese el primer lado: "));
-l2=float(input("Ingrese el segundo lado: "));
-l3=float(input("Ingrese el tercer lado: "));
-p=l1+l2+l3;
+print("Se va a calcular el perímetro del triángulo")
+l1=float(input("Ingrese el primer lado: "))
+l2=float(input("Ingrese el segundo lado: "))
+l3=float(input("Ingrese el tercer lado: "))
+p=l1+l2+l3
 print(f"El perímetro del triángulo es: {p}")
